64b914f10c4afbd550151fb86717e55e5552b07b
lestat.la2@gmail.com <lestat.la2@gmail.com>
2023-03-01 23:18:34 -0300

    video

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [ 
+    "./pages/**/*.{html,js,tsx,ts,jsx}",
+    "./components/**/*.{html,js,tsx,ts,jsx}",
+    
+ ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
