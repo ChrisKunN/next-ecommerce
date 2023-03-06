eda4b4f663aef8fa8bc4ba6085515a17c888529b
lestat.la2@gmail.com <lestat.la2@gmail.com>
2023-03-06 09:46:11 -0300

    Admin dashboard screen

--- components/Layout.js
+++ components/Layout.js
@@ -119,6 +119,17 @@ export default function Layout({ title, children }) {
 
                     </Menu.Item>
 
+                    {session.user.isAdmin && (
+                      <Menu.Item>
+                        <DropdownLink
+                          className="dropdown-link"
+                          href="/admin/dashboard"
+                        >
+                          Admin Dashboard
+                        </DropdownLink>
+                      </Menu.Item>
+                    )}
+
                     <Menu.Item>
                       <Link
                         className="dropdown-link"
